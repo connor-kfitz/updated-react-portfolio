ae176404ccf2812adbe7395ac4ebc3891d91d0e2
connor-kfitz <connorkfitzsimmons@gmail.com>
2023-04-13 20:44:14 -0400

    Added Contact View

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from './components/nav'
 import Header from './views/header';
 import About from './views/about';
 import Projects from './views/projects';
+import Contact from './views/contact';
 
 import './App.css';
 import './styles/main.scss';
@@ -14,6 +15,7 @@ function App() {
         <Header/>
         <About/>
         <Projects/>
+        <Contact/>
       </main>
     </div>
   );
---
+++ src/views/contact.jsx
@@ -0,0 +1,23 @@
+export default function Contact() {
+
+    return (
+        <section className="contact">
+            <div className="contact-container">
+                <h2 className="contact-container__header">Contact</h2>
+                <div className="contact-container__phone-number">
+                    Cell: <span className="contact-container__phone-number-text">289-200-5210</span>
+                </div>
+                <div className="contact-container__email">
+                    Email: <span className="contact-container__email-text">connorkfitzsimmons@gmail.com</span>
+                </div>
+                <form className="contact-form">
+                    <input type="text" className="contact-form__name" placeholder="Name"></input>
+                    <input type="text" className="contact-form__email" placeholder="Email"></input>
+                    <input type="text" className="contact-form__subject" placeholder="Subject"></input>
+                    <textarea className="contact-form__message" placeholder="Message"></textarea>
+                </form>
+                <button className="contact-form__send-button">Send</button>
+            </div>
+        </section>
+    )
+}
