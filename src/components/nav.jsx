c1127c3b8359df63456ccc470ced6a778257cfa1
connor-kfitz <connorkfitzsimmons@gmail.com>
2023-05-07 15:07:05 -0400

    Added Mobile Nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,13 +2,16 @@ import * as Constants from '../constants/constants';
 import Logo from '../images/PortfolioLogo.png';
 import mobileMenu from '../images/menu-icon.png';
 import { useEffect, useState, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 export default function Nav() {
 
-    const [navBackground, setNavBackground] = useState('transparent');
+    const [navBackground, setNavBackground] = useState(false);
     const [currentView, setCurrentView] = useState([0, true]);
     const [navLinkHover, setNavLinkHover] = useState(-1)
     const [socialLinkHover, setSocialLinkHover] = useState(-1);
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
     const scrollPosition = useRef(-1);
     const direction = useRef(true);
 
@@ -24,16 +27,12 @@ export default function Nav() {
         })
     },[])
 
-    const navStyle = {
-        backgroundColor: navBackground
-    }
-
     function updateNavBackgroundColour() {
         var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
         if (scrollTop > 0) {
-            setNavBackground('#00060f');
+            setNavBackground(true);
         } else {
-            setNavBackground('transparent');
+            setNavBackground(false);
         }
     }
 
@@ -85,8 +84,12 @@ export default function Nav() {
         setSocialLinkHover(hoverRef);
     }
 
+    function toggleMobileMenu() {
+        setShowMobileMenu(previous => !previous); 
+    }
+
     return (
-        <nav className="nav" id="nav" style={navStyle}>
+        <nav className="nav" id="nav" style={{backgroundColor: (navBackground || showMobileMenu) ? '#00060f' : 'transparent'}}>
             <img className="nav__logo" src={Logo} alt="Logo"/>
             <ul className="nav__link-box">
                 {Constants.navLinks.map((link, key) => (
@@ -104,14 +107,29 @@ export default function Nav() {
                     </li>
                 ))}
             </ul>
-            <div className="nav__mobile-menu">
+            <div className="nav__mobile-menu" onClick={() => toggleMobileMenu()}>
                 <img className="nav__mobile-menu-icon" src={mobileMenu}/>
             </div>
+            
+                <CSSTransition in={showMobileMenu} unmountOnExit timeout={500} classNames={"mobile-menu-overlay"}>
+                    <div className="nav__mobile-menu-overlay" onClick={() => toggleMobileMenu()}>
+                        <ul className="nav__mobile-link-box">
+                            {Constants.navLinks.map((link) => (
+                                <li className="nav__mobile-item">
+                                    <a className="nav__mobile-link" href={link.reference}>
+                                        {link.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </CSSTransition>
+                
             <ul className="nav__social-box">
                 {Constants.socialLinks.map((item, key) => (
                     <li className="nav__item" onMouseEnter={() => handleSocialHoverEffect(key)} onMouseLeave={() => handleSocialHoverEffect(-1)}>
                         <a className="nav__link" href={item.reference} target="_blank" rel="noopener noreferrer">
-                            <img class="nav__social-icon" src={(key === socialLinkHover) ? item.hover : item.icon} alt={`${item.name} icon`}
+                            <img className="nav__social-icon" src={(key === socialLinkHover) ? item.hover : item.icon} alt={`${item.name} icon`}
                             style={{filter: (key === socialLinkHover) ? null : 'invert(100%) sepia(100%) saturate(0%) hue-rotate(52deg) brightness(106%) contrast(101%)'}}
                             />
                         </a>
