bf6f338b8a9bc6c49885066fd013dd092f7c538c
connor-kfitz <connorkfitzsimmons@gmail.com>
2023-05-13 16:09:02 -0400

    Animated Contact Form

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -1,7 +1,13 @@
-import { useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+    const [animateInputField, setAnimateInputField] = useState(false);
 
     const form = useRef();
 
@@ -9,15 +15,58 @@ export default function Contact() {
     const templateId = process.env.REACT_APP_TEMPLATE_ID;
     const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 
+    useEffect(() => {
+        setTimeout(() => {
+            if (animateInputField) setAnimateInputField(false);
+        }, 300)
+    },[animateInputField])
+
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm(serviceId, templateId, form.current, publicKey)
+        if (checkInputFields()) {
+            emailjs.sendForm(serviceId, templateId, form.current, publicKey)
             .then((result) => {
+                clearForm();
                 console.log(result.text);
             }, (error) => {
                 console.log(error.text);
             });
+        }
+    }
+
+    function updateInputField(type) {
+        if (type === 'name') {
+            setName(form.current.name.value);
+        } else if (type ==='email') {
+            setEmail(form.current.email.value);
+        } else if (type === 'subject') {
+            setSubject(form.current.subject.value);
+        } else if (type === 'message') {
+            setMessage(form.current.message.value);
+        }
+    }
+
+    function checkInputFields() {
+        var sendEmail = true
+        if (!name) {
+            sendEmail = false;
+        }
+        if (!email) {
+            sendEmail = false;
+        }
+        if (!subject) {
+            sendEmail = false;
+        }
+        if (!message) {
+            sendEmail = false;
+        }
+        setAnimateInputField(true)
+        return sendEmail
+    }
+
+    function clearForm() {
+        form.current.reset();
     }
 
     return (
@@ -31,10 +80,14 @@ export default function Contact() {
                     Email: <span className="contact-container__email-text">connorkfitzsimmons@gmail.com</span>
                 </div>
                 <form className="contact-form" ref={form}>
-                    <input type="text" name="name" className="contact-form__name" placeholder="Name"></input>
-                    <input type="text" name="email" className="contact-form__email" placeholder="Email"></input>
-                    <input type="text" name="subject" className="contact-form__subject" placeholder="Subject"></input>
-                    <textarea name="message" className="contact-form__message" placeholder="Message"></textarea>
+                    <input type="text" name="name" className={`contact-form__name ${name ? 'contact-form__missing-info' : null} ${(!name && animateInputField) ? 'contact-form__animate' : null}`} 
+                           placeholder="Name" onChange={() => updateInputField('name')}></input>
+                    <input type="text" name="email" className={`contact-form__email ${email ? 'contact-form__missing-info' : null} ${(!email && animateInputField) ? 'contact-form__animate' : null}`} 
+                           placeholder="Email" onChange={() => updateInputField('email')}></input>
+                    <input type="text" name="subject" className={`contact-form__subject ${subject ? 'contact-form__missing-info' : null} ${(!subject && animateInputField) ? 'contact-form__animate' : null}`}
+                           placeholder="Subject" onChange={() => updateInputField('subject')}></input>
+                    <textarea name="message" className={`contact-form__message ${message ? 'contact-form__missing-info' : null} ${(!message && animateInputField) ? 'contact-form__animate' : null}`} 
+                              placeholder="Message" onChange={() => updateInputField('message')}></textarea>
                 </form>
                 <button className="contact-form__send-button" onClick={sendEmail}>Send</button>
             </div>
